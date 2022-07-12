739bde5bfe4c29f5792312c2d23f269341c68d9c
TL <TL@gmail.com>
2022-07-12 11:00:38 +0530

    Addition

--- Project01/src/com/quest/Program1.java
+++ Project01/src/com/quest/Program1.java
@@ -5,7 +5,12 @@ public class Program1 {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Hello World 1");
-
+		
+		int a=10;
+		int b=20;
+		
+		int c = a+b;
+		System.out.println("C :: "+c);
 	}
 
 }
